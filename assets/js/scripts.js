0f0da5ea6f6f822ab840cf9777c874db91380ecd
andrea sicilia <andreasiciliag@gmail.com>
2024-02-08 16:48:06 -0300

    proyecto final

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,28 @@
-console.log('Hola mundo')
+
+$(document).ready(function () {
+
+    //Tooltip
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    //Evento
+    $('#enviarCorreo').on('click', function () {
+        alert('El correo fue enviado correctamente...')
+    })
+
+    // Selector de etiqueta
+    $('h2.ingredientes, h2.preparacion').on('dblclick', function () {
+        $(this).css({
+            "color": "red",
+        });
+    })
+    $(".text-toggle").click(function () {
+        $(".card").toggle();
+    });
+
+    $(".aparecer").click(function () {
+        $(".card").toggle();
+    });
+
+})
